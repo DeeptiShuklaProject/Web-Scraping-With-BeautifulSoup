9e7839c468f8c1ac15fc3af34991849ce6f63343
DeeptiShuklaProject <deeptishukla515@gmail.com>
2024-06-16 23:51:56 +0530

    IPL & flipkart CSV file using web Scrapping BeautifulSoup

---
+++ flip.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+Product_name = []
+Prices = []
+Description = []
+Reviews = []
+
+for i in range(2, 12):
+    url = "https://www.flipkart.com/search?q=mobiles+under+50000&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off&page=" + str(i)
+
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "lxml")
+    box = soup.find("div", class_="DOjaWF gdgoEp")
+
+    if box:
+        names = box.find_all("div", class_="KzDlHZ")
+        prices = box.find_all("div", class_="Nx9bqj _4b5DiR")
+        descs = box.find_all("ul", class_="G4BRas")
+        reviews = box.find_all("div", class_="XQDdHH")
+
+        for name, price, desc, review in zip(names, prices, descs, reviews):
+            Product_name.append(name.text)
+            Prices.append(price.text)
+            Description.append(desc.text)
+            Reviews.append(review.text)
+
+df = pd.DataFrame({"Product Name": Product_name, "Prices": Prices, "Description": Description, "Reviews": Reviews})
+print(df)
+
+df.to_csv("C:/Users/Avinesh Kumar/Documents/web_scapping/flipkart_mobiles_under_50000.csv")
